0d8ce4890b3dea33120f1122011e38be8482d2b1
a.kudrinskiy <artem.tankist.2015@yandex.ru>
2024-05-14 11:25:43 +0300

    Изменение обработки текстов, изменение векторизации

--- main.py
+++ main.py
@@ -2,86 +2,122 @@ import string
 import os
 import time
 
+import axis as axis
+import numpy as np
+import spacy
+import numpy
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from collections import Counter
 import nltk
 import pymorphy2
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import LogisticRegression
-
 
 stop_words = set(stopwords.words('russian'))
 punctuation = set(string.punctuation)
 morph = pymorphy2.MorphAnalyzer()
 
+nlp = spacy.load("ru_core_news_sm")
+
+
+def process_text(text):
+    # Токенизация
+    tokens = word_tokenize(text, language='russian')
+
+    # Приводим слова к нижнему регистру, удаляем стоп-слова и знаки пунктуации
+    filtered_tokens = [word.lower() for word in tokens
+                       if word.isalpha() and word.lower() not in stop_words and word not in punctuation]
+
+    # Приводим слова к начальной форме
+    lemmas = [morph.parse(token)[0].normal_form for token in filtered_tokens]
+
+    # Обрабатываем текст с помощью nlp модели
+    doc = nlp(' '.join(lemmas))
+
+    # Получаем список слов, частей речи и дерево зависимостей для каждого токена
+    processed_text, token_pos, token_dep = ([token.text for token in doc],
+                                            [token.pos_ for token in doc],
+                                            [token.dep_ for token in doc])
+
+    return processed_text, token_pos, token_dep
+
+
+def count_word_occurrences(lemmas):
+
+    # Удаляет леммы, но не удаляет части речи и уровень зависимости
+    # Удаляет леммы, но не удаляет части речи и уровень зависимости
+    # Удаляет леммы, но не удаляет части речи и уровень зависимости
+    # Удаляет леммы, но не удаляет части речи и уровень зависимости
+
+    # Порог для минимального количества встречаемости слова в тексте
+    word_count_threshold = 0
+
+    # Считаем количество вхождений каждого слова
+    word_counts = Counter(lemmas)
+
+    # Удаляем слова, которые встречаются редко
+    filtered_lemmas = [word for word in lemmas if word_counts[word] >= word_count_threshold]
+
+    return filtered_lemmas
+
 
 def dict_text(directory):
     file_data = {}
+    file_token_pos = []
+    file_token_dep = []
+
     for file_name in os.listdir(directory):
-        current_time = time.time()
         with open(directory + '/' + file_name, 'r', encoding='UTF-8-SIG') as file:
             lines = file.readlines()
+
             author = lines[0].strip()
             text = ''.join(lines[2:])
 
-            tokens = word_tokenize(text, language='russian')
-            filtered_tokens = [word.lower() for word in tokens
-                               if word.isalpha() and word.lower() not in stop_words and word not in punctuation]
+            lemmas, token_pos, token_dep = process_text(text)
 
-            lemmas = [morph.parse(token)[0].normal_form for token in filtered_tokens]
+            # filtered_lemmas = count_word_occurrences(lemmas)
+
+            filtered_lemmas = lemmas
+
+            file_token_pos += [' '.join(text for text in token_pos)]
+            file_token_dep += [' '.join(text for text in token_dep)]
 
             if author not in file_data:
                 file_data[author] = []
 
-            file_data[author] = lemmas
+            file_data[author].append(filtered_lemmas)
 
-            print("Текст %s просмотрен за %s секунд" % (lines[1][::], time.time() - current_time))
-
-    return file_data
+    return file_data, file_token_pos, file_token_dep
 
 
-start_time = time.time()
-result = dict_text("data/training")
-print("Все тексты просмотрены за %s секунд!" % (time.time() - start_time))
+training_data, training_token_pos, training_token_dep = dict_text("data/training")
+authors = [index for index, author in enumerate(training_data) for _ in training_data[author]]  # Разделение текстов по авторам
+authors_index = {author: index for index, author in enumerate(training_data)}  # Индексы авторов
+final_training_data = [' '.join(text) for author_texts in training_data.values() for text in author_texts]  # Тексты
 
-# def list_text(directory):
-#     file_data = []
-#     for root, directories, filenames in os.walk(directory):
-#         for filename in filenames:
-#             file_path = os.path.join(root, filename)
-#             with open(file_path, 'r', encoding='utf-8') as file:  # Считываем даннные из файла
-#                 text = file.read()
-#
-#                 # Токенизация
-#                 tokens = word_tokenize(text, language='russian')
-#                 # Приводим слова к нижнему регистру, удаляем стоп-слова и знаки пунктуации
-#                 filtered_tokens = [word.lower() for word in tokens if
-#                                    word.isalpha() and word.lower() not in stop_words and word not in punctuation]
-#
-#                 morph = pymorphy2.MorphAnalyzer()
-#                 # Приводим слова к начальной форме
-#                 lemmas = [morph.parse(token)[0].normal_form for token in filtered_tokens]
-#
-#                 file_data.append(lemmas)
-#
-#     return file_data
+print(authors_index)
 
+test_data, test_token_pos, test_token_dep = dict_text('data/testData')
+final_test_data = [' '.join(text) for author_texts in test_data.values() for text in author_texts]
 
-# training_data = list_text('data/training/Pushkin')
-# final_training_data = [' '.join(text) for text in training_data]
-#
-# test_data = list_text('data/test/Pushkin')
-# final_test_data = [' '.join(text) for text in test_data]
+tfidf = TfidfVectorizer()
+text_features = tfidf.fit_transform(final_training_data).toarray()
+training_pos_features = tfidf.transform(training_token_pos).toarray()
+training_dep_features = tfidf.transform(training_token_dep).toarray()
 
-# authors = [3, 2, 3, 1, 1, 3, 2, 3]
+training_all_features = np.concatenate((text_features, training_pos_features, training_dep_features), axis=1)
+classifier = LogisticRegression()
+classifier.fit(training_all_features, authors)
 
-# tfidf = TfidfVectorizer()
-# X = tfidf.fit_transform(final_training_data)
-#
-# classifier = LogisticRegression()
-# classifier.fit(X, authors)
-#
-# new_X = tfidf.transform(final_test_data)
-# predictions = classifier.predict(new_X)
-#
-# print(predictions)
+test_text_features = tfidf.transform(final_test_data).toarray()
+test_pos_features = tfidf.transform(test_token_pos).toarray()
+test_dep_features = tfidf.transform(test_token_dep).toarray()
+
+test_all_features = np.concatenate((test_text_features, test_pos_features, test_dep_features), axis=1)
+new_count_vectorizer = tfidf.transform(final_test_data)
+
+predictions = classifier.predict(test_all_features)
+
+print(predictions)
